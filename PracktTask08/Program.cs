dd0aa1138a40901ec5616c2cc12e21442ba8185d
УринбасаровД <@A-305-10>
2017-12-11 14:17:31 +0600

    first

---
+++ PracktTask08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracktTask08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray tempArr = new RangeOfArray(2, 5);
+
+            for (int i = 0; i < 10; i++)
+            {
+               Console.WriteLine("Element #{0} = {1}", i, tempArr[i]);
+            }
+        }
+    }
+}
---
+++ PracktTask08/RangeOfArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracktTask08
+{
+    class RangeOfArray
+    {
+        private readonly int minIndex;
+        private readonly int maxIndex;
+        private readonly int[] array;
+
+        public RangeOfArray(int minIndex, int maxIndex)
+        {
+            this.minIndex = minIndex;
+            this.maxIndex = maxIndex;
+            array = new int[maxIndex - minIndex + 1];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index - minIndex];
+            }
+            set
+            {
+                array[index - minIndex] = value;
+            }
+        }
+    }
+}
