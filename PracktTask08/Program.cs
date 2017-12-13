c9bab5dc1d1e754f141d51dac25b4794dc59e3c3
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-12-14 00:33:57 +0600

    first

--- PracktTask08/Program.cs
+++ PracktTask08/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PracktTask08
 {
@@ -10,16 +6,92 @@ namespace PracktTask08
     {
         static void Main(string[] args)
         {
-            RangeOfArray tempArr = new RangeOfArray(2, 11);
+            const int MIN_NUM = -100;
+            const int MAX_NUM = 200;
 
-            //for (int i = 0; i < 10; i++)
-            //{
-            //   Console.WriteLine("Element #{0} = {1}", i, tempArr[5]);
-            //}
+            int minIndex;
+            Console.WriteLine("Введите минимальный диапазон массив: ");
+            bool isParcedmin = int.TryParse(Console.ReadLine(), out minIndex);
+            if (!isParcedmin)
+            {
+                Console.Write("Ошибка!");
+            }
 
-            Console.WriteLine(tempArr[10]);
+            int maxIndex;
+            Console.WriteLine("Введите максимальный диапазон массива: ");
+            bool isParcedmax = int.TryParse(Console.ReadLine(), out maxIndex);
+            if(!isParcedmax)
+            {
+                Console.Write("Ошибка!");
+            }
 
-            Console.ReadLine();
+            RangeOfArray array = new RangeOfArray(minIndex, maxIndex);
+
+            Random rnd = new Random();
+
+            if(minIndex < maxIndex)
+            {
+                Console.WriteLine("\n");
+                for (int i = minIndex; i < maxIndex; i++)
+                    {
+                        array[i] = rnd.Next(MIN_NUM, MAX_NUM);
+                        Console.WriteLine("{0} - {1}", i, array[i]);
+                    }
+            }
+            else
+            {
+                    for (int i = maxIndex; i < minIndex; i++)
+                    {
+                        array[i] = rnd.Next(MIN_NUM, MAX_NUM);
+                        Console.WriteLine("{0} - {1}", i, array[i]);
+                    }
+            }
+            Console.WriteLine();
+
+            int index;
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите индекс элемента массива: ");
+                    bool isParcedIndex = int.TryParse(Console.ReadLine(), out index);
+                    if(!isParcedIndex)
+                    {
+                        Console.WriteLine("Ошибка!");
+                    }
+                    else
+                    {
+                        if(minIndex <maxIndex)
+                        {
+                            for (int j = minIndex; j < maxIndex; j++)
+                            {
+                                for (int i = minIndex; i < maxIndex; i++)
+                                {
+                                   // Console.WriteLine("{0} - {1}", j ,array[i]);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int j = minIndex; j < maxIndex; j++)
+                            {
+                                for (int i = maxIndex; i < minIndex; i++)
+                                {
+                                   // Console.Write("{0} - {1}", j,  array[i]);
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+
+                        Console.WriteLine("Элемент с индексом {0} = {1}", index, array[index]);
+                    }
+                }
+                catch (IndexOutOfRangeException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
--- PracktTask08/RangeOfArray.cs
+++ PracktTask08/RangeOfArray.cs
@@ -1,33 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PracktTask08
 {
     class RangeOfArray
     {
-        private  int minIndex;
-        private  int maxIndex;
-        private  int[] array;
+        private int minIndex;
+        private int maxIndex;
+        private int[] array;
 
         public RangeOfArray(int minIndex, int maxIndex)
         {
             this.minIndex = minIndex;
             this.maxIndex = maxIndex;
-            array = new int[maxIndex - minIndex + 1];
+            if (minIndex > maxIndex)
+                array = new int[minIndex - maxIndex];
+            else
+                array = new int[maxIndex - minIndex];
         }
 
         public int this[int index]
         {
-            get => array[index - minIndex];
             set
             {
-                array[index] = value;
+                if (minIndex < maxIndex)
+                {
+                    if (index < minIndex || index > maxIndex)
+                        throw new IndexOutOfRangeException("Индекс вне границ массива!");
+                    array[index - minIndex] = value;
+                }
+                else
+                {
+                    if (index > minIndex || index < maxIndex)
+                        throw new IndexOutOfRangeException("Индекс вне границ массива!");
+                    array[index - maxIndex] = value;
+                }
             }
+            get
+            {
+                if (minIndex < maxIndex)
+                {
+                    if (index < minIndex || index > maxIndex)
+                        throw new IndexOutOfRangeException("Индекс вне границ массива!");
+                    return array[index - minIndex];
+                }
+                else
+                {
+                    if (index > minIndex || index < maxIndex)
+                        throw new IndexOutOfRangeException("Индекс вне границ массива!");
+                    return array[index - maxIndex];
+                }
+
+            }
+
+
         }
-
-
     }
 }
